ed4b11d018fbc29759eadf21a4090c12d511fadb
anandhuc1666 <anandhuc166@gmail.com>
2025-10-14 15:16:22 +0530

    Mondodb

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,23 @@ const User = require('../models/user')
 
 //create a new users in post:
 
-router.post('/',async(req,res)=>{
-    try{
+router.post('/', async (req, res) => {
+      const { name, email, number } = req.body
+    try {
+      const user = new User({name,email,number})
+      await user.save()
+      res.status(200).json({message: "user created successfully"})
+    } catch(err){
+    res.status(404).json({err: err.message});
 
-    }catch{
-        
+    }
+})
+
+router.get('/',async(req,res)=>{
+    try{
+   const users = await User.find()
+   res.status(200).json(users)
+    }catch(err){
+        res.status(404).json({err:err.message})
     }
 })
