e634817de93cdc7c4cd763437107b7010e92c8e7
anandhuc1666 <anandhuc166@gmail.com>
2025-10-14 14:51:19 +0530

    mongodb

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const app = express()
 mongoose
 .connect(process.env.MONGODB_URL)
 .then(()=>console.log('connected db'))
-.catch(()=>console.log('server not connected db'))
+.catch((err)=>console.log('server not connected db',err))
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT,()=>console.log(`server is on running ${PORT}`))
