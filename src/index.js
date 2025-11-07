f18ca2c621e246e1ffd5f104966c4fd5b3bf1c05
anandhuc1666 <anandhuc166@gmail.com>
2025-11-07 09:20:30 +0530

    mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import express from "express"
-import dotenv from "dotenv"
-import route from "./router/userRoute.js"
-import Newmongoose from "./mongoose.js"
+import express from "express";
+const app = express();
+app.use(express.json());
+const server = [];
 
-const app = express()
-app.use(express.json())
-dotenv.config()
+app.post("/create", (req, res) => {
+  const { email, password, name } = req.body;
+  const lengthId = server.length;
+  const newUser = {
+    id: lengthId + 1,
+    name,
+    email,
+    password,
+  };
+  server.push(newUser);
+  res.json(newUser);
+});
+app.get("/resive", (req, res) => {
+  const user = server.map((item) => ({
+    id: item.id,
+    name: item.name,
+    email: item.email,
+    password: item.password,
+  }));
+  res.json(user);
+});
+app.patch("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  const user = server.find((item) => item.id == id);
+  if (name) user.name = name;
+  if (email) user.email = email;
+  if (password) user.password = password;
+  res.json(user);
+});
 
-app.use("/user",route)
-Newmongoose()
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT,()=>console.log(`server is running on ${PORT}`))
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
--- src/router/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { login, register, salaryCheck } from "../controller/userController.js";
-import verifyuser from "../middleware/auth.js";
-
-const route = Router();
-
-route.post("/register", register).post("/login",login).get("/salary",salaryCheck)
-
-export default route;
